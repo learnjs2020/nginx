7f5b71283cda55d60be8b51a601bff0f93fc9016
learnjs2020 <54968697+learnjs2020@users.noreply.github.com>
2020-07-20 16:57:02 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,4 +12,4 @@ docker network create \
 docker build -t some-content-nginx .
 docker stop some-nginx
 docker rm some-nginx
-docker run --name some-nginx -v "$(pwd)/html":/usr/share/nginx/html -p 80:80 -d some-content-nginx
+docker run --name some-nginx --network network_ui_app -v "$(pwd)/html":/usr/share/nginx/html -p 80:80 -d some-content-nginx
