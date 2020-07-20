e57abb3014b66394f44ea6747ab9a2045ae822e6
learnjs2020 <54968697+learnjs2020@users.noreply.github.com>
2020-07-20 16:37:05 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ git pull
 docker build -t some-content-nginx .
 docker stop some-nginx
 docker rm some-nginx
-docker run --name some-nginx -v "$(pwd)/html":/usr/share/nginx/html -p 8080:80 -d some-content-nginx
+docker run --name some-nginx -v "$(pwd)/html":/usr/share/nginx/html -p 80:80 -d some-content-nginx
